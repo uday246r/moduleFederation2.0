1bffe83603011a95558a31d1daa764ffa90ee4ac
Uday246r <udaychauhan246r@gamil.com>
2026-07-01 22:33:58 +0530

    testing nx and without nx pipline 2

--- shared/src/index.jsx
+++ shared/src/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function NxButton({ label = 'Nx Button' }) {
   return (
      <button style={{ padding: '10px 20px', backgroundColor: '#0052CC', color: 'white', borderRadius: '4px', border: 'none', cursor: 'pointer' }}>
-      {label}
+      {label} - updated
     </button>
   );
 }
